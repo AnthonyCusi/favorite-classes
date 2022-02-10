541bc93d5ea8b67a2946deb1b0fd19137d506fef
Anthony Cusimano <anthonyc.cusimano@gmail.com>
2022-02-10 12:50:28 -0800

    Created class adding system, added styling

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import Class from "../Class/Class"
+import "./Home.css"
+
+
+function Home(props) {
+
+    const [value, setValue] = useState('')
+    const [favoriteClasses, setClasses] = useState([]);
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!favoriteClasses.includes(value)) {
+            setClasses(favoriteClasses.concat(value));
+            setValue('');
+        }
+
+        console.log(favoriteClasses)
+    }
+
+    return (
+        <body>
+            <div className="header">
+                <h1>Anthony's Favorite Classes</h1>
+                <form onSubmit={handleSubmit}>
+                    <label>Add a Class </label>
+                    <input type="text" value={value} onChange={handleChange}></input>
+                    <button type="submit">Add Class!</button>
+                </form>
+            </div>
+            <div className="my-classes">
+                {favoriteClasses.map((favClass) => 
+                    <Class name={favClass} key={favClass}></Class>
+                )}
+            </div>
+        </body>
+    
+        
+    )
+}
+
+export default Home;
