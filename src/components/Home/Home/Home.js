a829d1e68af706111d35704d34ce803a8dbf419e
Anthony Cusimano <anthonyc.cusimano@gmail.com>
2022-02-17 23:54:39 -0800

    Implemented PeterPortal API to get class info

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Class from "../Class/Class"
+import ClassGraphQL from '../Class/ClassGraphQL';
 import "./Home.css"
 
 
@@ -39,6 +40,11 @@ function Home(props) {
             <div className="my-classes">
                 {favoriteClasses.map((favClass) => 
                     <Class name={favClass} key={favClass}></Class>
+                )}
+            </div>
+            <div className="my-classes">
+                {favoriteClasses.map((favClass) => 
+                    <ClassGraphQL name={favClass} key={favClass}></ClassGraphQL>
                 )}
             </div>
         </body>
